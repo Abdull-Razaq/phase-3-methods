7570a37fc9b7aaa6e91cd2a3a92337a0075b8a3e
Abdirizak Mohamed <abdirizakm123@gmail.com>
2023-02-13 18:26:19 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+
+# First methods
+def greet_programmer 
+    puts "Hello, programmer!"
+ends
+
+
+# Second method
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+
+# Third method
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}"
+end
+
+
+# Fourth method
+def add (num1, num2)
+    return num1 + num2
+end
+
+# Fifth method
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    return number / 2
+end
+
+
+
+
